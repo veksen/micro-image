b67b651ef3285dd0494356f8ddbb5a2485500e03
Jean-Philippe Sirois <jp@veksen.net>
2023-09-17 10:08:10 -0400

    docs: bootstrap docs examples

--- apps/docs/src/pages/index.tsx
+++ apps/docs/src/pages/index.tsx
@@ -5,16 +5,38 @@ export default function Docs() {
   useIsomorphicLayoutEffect(() => {
     console.log("micro-image docs page");
   }, []);
+
   return (
     <div>
       <h1>micro-image Documentation</h1>
-      <Image
-        src="https://picsum.photos/id/237/800/500"
-        width={800}
-        height={500}
-        alt=""
-        objectFit="cover"
-      />
+
+      <div style={{ maxWidth: "800px" }}>
+        <h2>Local image using micro-image proxy</h2>
+        <p>
+          Using `http://localhost:3002/sniffa.gif`, meaning hosted inside Next.js `/public` folder:
+        </p>
+        <Image
+          src="http://localhost:3002/sniffa.gif"
+          width={800}
+          height={500}
+          alt=""
+          objectFit="cover"
+        />
+
+        <h2>Remote image using micro-image proxy</h2>
+        <p>Using `https://picsum.photos/1500/900`:</p>
+        <Image
+          src="https://picsum.photos/1500/900"
+          width={800}
+          height={500}
+          alt=""
+          objectFit="cover"
+        />
+
+        <h2>Image from ipx</h2>
+        <p>Using `bliss.jpg`, stored inside ipx assets:</p>
+        <Image src="bliss.jpg" width={800} height={500} alt="" objectFit="cover" />
+      </div>
     </div>
   );
 }
