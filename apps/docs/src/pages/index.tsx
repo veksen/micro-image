ff1c40a060be36909624e2f98ef0ea86a5c87fc1
Jean-Philippe Sirois <jp@veksen.net>
2023-09-24 01:35:15 -0400

    docs: update docs to use render's url

--- apps/docs/src/pages/index.tsx
+++ apps/docs/src/pages/index.tsx
@@ -1,4 +1,4 @@
-import Image from "@micro-image/image";
+import Image, { ImageCacheProvider } from "@micro-image/image";
 
 export default function Docs() {
   return (
@@ -6,29 +6,35 @@ export default function Docs() {
       <h1>micro-image Documentation</h1>
 
       <div style={{ maxWidth: "800px" }}>
-        <h2>Local image using micro-image proxy</h2>
-        <p>
-          Using `http://localhost:3002/sniffa.gif`, meaning hosted inside Next.js `/public` folder:
-        </p>
-        <Image
-          src="http://localhost:3002/sniffa.gif"
-          width={800}
-          height={500}
-          alt=""
-          objectFit="cover"
-        />
+        <ImageCacheProvider
+          provider="micro-image"
+          cacheProxyUrl="https://micro-image-cache.onrender.com"
+        >
+          <h2>Local image using micro-image proxy</h2>
+          <p>
+            Using `http://localhost:3002/sniffa.gif`, meaning hosted inside Next.js `/public`
+            folder:
+          </p>
+          <Image
+            src="http://localhost:3002/sniffa.gif"
+            width={800}
+            height={500}
+            alt=""
+            objectFit="cover"
+          />
 
-        <h2>Remote image using micro-image proxy</h2>
-        <p>Using `https://picsum.photos/1500/900`:</p>
-        <Image
-          src="https://picsum.photos/1500/900"
-          width={800}
-          height={500}
-          alt=""
-          objectFit="cover"
-        />
+          <h2>Remote image using micro-image proxy</h2>
+          <p>Using `https://picsum.photos/1500/900`:</p>
+          <Image
+            src="https://picsum.photos/1500/900"
+            width={800}
+            height={500}
+            alt=""
+            objectFit="cover"
+          />
+        </ImageCacheProvider>
 
-        <h2>Image from ipx</h2>
+        <h2>(TODO): Image from ipx</h2>
         <p>Using `bliss.jpg`, stored inside ipx assets:</p>
         <Image src="bliss.jpg" width={800} height={500} alt="" objectFit="cover" />
       </div>
